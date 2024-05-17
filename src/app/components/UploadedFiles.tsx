da3c340ca83a9404a9dfe447f2b57335e2ef85cd
Isah Ohieku <isahohieku@gmail.com>
2024-05-17 16:28:59 +0100

    Created uploaded files aggregator module

---
+++ src/app/components/UploadedFiles.tsx
@@ -0,0 +1,9 @@
+import FileUploadProgress from './FileUploadProgress';
+import { useFilesUpload } from '../contexts/filesUploaderContext';
+
+
+export default function UploadedFiles() {
+  const { allFiles } = useFilesUpload();
+
+    return (allFiles.map(file => <FileUploadProgress {...file} key={`${file.file.name}-${file.file.lastModified}`} />))
+  }
