b3e99a409ef98b5f40defc9cf1ffa299c169703e
Isah Ohieku <isahohieku@gmail.com>
2024-05-17 16:30:11 +0100

    Create http request handler lib

---
+++ src/app/libs/api/index.ts
@@ -0,0 +1,37 @@
+import type { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import _axios from 'axios';
+import { API_BASE_URL } from '../../app-constants';
+
+export const axios: AxiosInstance = _axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const handleApiSuccess = <T>(res: AxiosResponse<T>): T => {
+  return res.data;
+};
+
+const handleApiError = (error: AxiosError): void => {
+  let errorMessage = '';
+
+  if (!error.status) {
+    errorMessage = error.message;
+  }
+
+  if (_axios.isCancel(error)) {
+    return; // Fail silently
+  }
+
+  throw errorMessage;
+};
+
+export const Api = {
+  get: <T>(endpoint: string, config?: AxiosRequestConfig): Promise<T> =>
+    axios.get(endpoint, config).then(handleApiSuccess).catch(handleApiError),
+  post: <T, U>(endpoint: string, data: T, config?: AxiosRequestConfig): Promise<U> =>
+    axios.post(endpoint, data, config).then(handleApiSuccess).catch(handleApiError),
+  put: <T, U>(endpoint: string, data: T, config?: AxiosRequestConfig): Promise<U> =>
+    axios.put(endpoint, data, config).then(handleApiSuccess).catch(handleApiError),
+};
