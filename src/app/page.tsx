369ff35fdb0f6cd4b52e7e7140b71f37b544a636
Isah Ohieku <isahohieku@gmail.com>
2024-05-17 16:29:16 +0100

    Refactor main page module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 'use client'
-import { useCallback, useState } from 'react';
 import FileUploader from './components/FileUploader';
-import FileUploadProgress from './components/FileUploadProgress';
+import UploadedFiles from './components/UploadedFiles';
+import { FilesUploadProvider } from './contexts/filesUploaderContext';
 
 export default function Home() {
-
-  const [uploadProgress, setUploadProgress] = useState(0);
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 mt-10">
       <div className="space-y-2 text-center">
         <h2 className="text-2xl font-bold">Upload Your Files</h2>
         <p className="text-gray-500 dark:text-gray-400">Drag and drop your files or click to select.</p>
       </div>
-      <FileUploader />
-      <FileUploadProgress />
-      <FileUploadProgress />
-      <FileUploadProgress />
+      <FilesUploadProvider>
+        <FileUploader />
+        <UploadedFiles />
+      </FilesUploadProvider>
     </div>
   );
 }
