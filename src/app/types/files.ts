26d4a0894e71bcd916a2c2183c267b321e6ac161
Isah Ohieku <isahohieku@gmail.com>
2024-05-18 08:04:59 +0100

    Add status and improve upload file typings

--- src/app/types/files.ts
+++ src/app/types/files.ts
@@ -1,6 +1,24 @@
-export type UploadedFile = { file: File, progress: number, status: boolean };
+import { AxiosProgressEvent } from "axios";
+
+export enum UploadStatus {
+    NOT_STARTED = 'not_started',
+    STARTED = 'started',
+    COMPLETED = 'completed',
+    FAILED = 'failed'
+}
+
+export type UploadedFile = { 
+    file: File;
+    progress: number;
+    status: UploadStatus;
+    id: string;
+    taskId: string | null;
+ };
 
 export type FilesUploadContextProps = {
   allFiles: UploadedFile[];
   handleOnFilesUploaded(files: File[]): void;
 };
+
+
+export type AxiosOnProgress = ((progressEvent: AxiosProgressEvent) => void) | undefined;
