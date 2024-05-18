d076ab8cb70b390adae0d789c8695dc4bce80513
Isah Ohieku <isahohieku@gmail.com>
2024-05-18 08:06:26 +0100

    Add new handler methods for upload process

--- src/app/contexts/filesUploaderContext.tsx
+++ src/app/contexts/filesUploaderContext.tsx
@@ -1,20 +1,88 @@
 'use client'
-import { createContext, useContext, useState } from 'react';
-import { FilesUploadContextProps, UploadedFile } from '../types/files';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { FilesUploadContextProps, UploadStatus, UploadedFile } from '../types/files';
+import { handleProcessingFileUpload } from '../services/dragonfly';
+import { AxiosProgressEvent } from 'axios';
 
 export const FilesUploadContext = createContext<FilesUploadContextProps | null>(null);
 
 export const FilesUploadProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [allFiles, setFiles] = useState<UploadedFile[]>([]);
 
+  const handleUpdateStartStatus = (id: string) => {
+    setFiles((files) => {
+      const cloneFiles = [...files];
+      const filePosition = getFilePosition(cloneFiles, id);
+      cloneFiles[filePosition].status = UploadStatus.STARTED;
+      return cloneFiles;
+    });
+  }
+
   const handleOnFilesUploaded = (files: File[]) => {
-    console.log({ files })
     setFiles((prevFiles) => {
       const newFiles: UploadedFile[] = [];
-      files.forEach(_file => newFiles.push({ file: _file, progress: 0, status: false }));
+      files.forEach(_file => newFiles.push({
+        file: _file,
+        progress: 0,
+        status: UploadStatus.NOT_STARTED,
+        id: uuidv4(),
+        taskId: null
+      }));
       return [...newFiles, ...prevFiles];
     });
   }
+
+  const getFilePosition = (files: UploadedFile[], id: string) => {
+    const cloneFiles = [...files];
+    return cloneFiles.findIndex(file => file.id = id);
+  }
+
+  const handleProgressUpdater = (progressEvent: AxiosProgressEvent, id: string) => {
+    const percentCompleted = Math.round((progressEvent.loaded * 100) / (progressEvent?.total || 0));
+    setFiles((files) => {
+      const cloneFiles = [...files];
+      const filePosition = getFilePosition(cloneFiles, id);
+      cloneFiles[filePosition].progress = percentCompleted;
+      return cloneFiles;
+    });
+  }
+
+  const handleOnProcessCompleted = (taskId: string, id: string) => {
+    setFiles((files) => {
+      const cloneFiles = [...files];
+      const filePosition = getFilePosition(cloneFiles, id);
+      cloneFiles[filePosition].taskId = taskId;
+      return cloneFiles;
+    });
+  }
+
+  const handleOnFailed = (id: string) => {
+    setFiles((files) => {
+      const cloneFiles = [...files];
+      const filePosition = cloneFiles.findIndex(file => file.id = id);
+      cloneFiles[filePosition].status = UploadStatus.FAILED;
+      return cloneFiles;
+    });
+  }
+
+  const uploadFile = () => {
+    const newFiles = allFiles.filter(({ status }) => status === UploadStatus.NOT_STARTED);
+
+    newFiles.forEach(({ file, id }) => {
+      handleUpdateStartStatus(id)
+      handleProcessingFileUpload(
+        file,
+        (progressEvent: AxiosProgressEvent) => handleProgressUpdater(progressEvent, id),
+        (taskId: string) => handleOnProcessCompleted(taskId, id)),
+        handleOnFailed
+    })
+
+  }
+
+  useEffect(() => {
+    uploadFile()
+  }, [allFiles]);
 
   return (
     <FilesUploadContext.Provider
